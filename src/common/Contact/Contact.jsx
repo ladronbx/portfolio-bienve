6616711c185d8565e5eb08f31843a665fa6093a2
ladronbx <ladronbravovlc@gmail.com>
2024-02-11 16:37:05 +0100

    [feat] : agrego links a las imágenes del contacto de la cabecera

--- src/common/Contact/Contact.jsx
+++ src/common/Contact/Contact.jsx
@@ -8,13 +8,19 @@ export const Contact = () => {
         <div className="flex justify-end">
             <div className="flex space-x-5 items-center">
                 <div className="contact-icon">
-                    <img className="2xl:h-[35px] xl:h-[35px]  lg:h-[30px] md:h-[22px] sm:h-[19px] xs:h-[17px]" src={mailIcon} alt="Mail Icon" draggable="false" />
+                    <a href="mailto:ladronbravovlc@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <img className="2xl:h-[35px] xl:h-[35px]  lg:h-[30px] md:h-[22px] sm:h-[19px] xs:h-[17px]" src={mailIcon} alt="Mail Icon" draggable="false" />
+                    </a>
                 </div>
                 <div className="contact-icon">
-                    <img className="2xl:h-[40px] xl:h-[40px] lg:h-[32px] md:h-[26px] sm:h-[24px] xs:h-[22px]" src={linkedinIcon} alt="LinkedIn Icon" draggable="false" />
+                    <a href="https://www.linkedin.com/in/ladronbx/" target="_blank" rel="noopener noreferrer">
+                        <img className="2xl:h-[40px] xl:h-[40px] lg:h-[32px] md:h-[26px] sm:h-[24px] xs:h-[22px]" src={linkedinIcon} alt="LinkedIn Icon" draggable="false" />
+                    </a>
                 </div>
                 <div className="contact-icon">
-                    <img className="2xl:h-[40px] xl:h-[40px] lg:h-[32px] md:h-[26px] sm:h-[24px] xs:h-[22px]" src={githubIcon} alt="GitHub Icon" draggable="false" />
+                    <a href="https://github.com/ladronbx" target="_blank" rel="noopener noreferrer">
+                        <img className="2xl:h-[40px] xl:h-[40px] lg:h-[32px] md:h-[26px] sm:h-[24px] xs:h-[22px]" src={githubIcon} alt="GitHub Icon" draggable="false" />
+                    </a>
                 </div>
             </div>
         </div>
