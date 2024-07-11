e84447dce3bb51ddcf7a6066e6f9c5114ee93fae
ladronbx <ladronbravovlc@gmail.com>
2024-07-11 22:00:59 +0200

    update july 2024

--- src/common/Intro/Intro.jsx
+++ src/common/Intro/Intro.jsx
@@ -6,7 +6,7 @@ export const Intro = () => {
             <h2 className='titles line-green' style={{ textShadow: '0 0 2px rgb(145, 246, 21)' }}>Hey there👋</h2>
             <div className="text-xl text-green-100 mt-2">
                 <p><strong>Soy Bienve, Full Stack Developer</strong></p>
-                <p>En 2022 dejé mi trabajo en una multinacional como administrativa y me lancé a estudiar programación. Desde entonces, me he dedicado a especializarme en el desarrollo de software.</p>
+                <p>En 2022 dejé mi trabajo en una multinacional como administrativa y me lancé a estudiar programación. Desde entonces, me he dedicado a especializarme en el desarrollo web.</p>
                 <p>Finalicé mis estudios en 2024 y, en marzo de ese mismo año, comencé a trabajar en el sector hasta la actualidad.</p>
                 <p>Me encanta aprender y seguir formándome, por lo que siempre estoy en constante evolución.</p>
                 <p>Si quieres saber más sobre mí, sigue bajando 👇</p>
