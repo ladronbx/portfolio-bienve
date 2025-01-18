1eedfb0827cd3e342700647d7c3c37178a6525f5
ladronbx <ladronbravovlc@gmail.com>
2025-01-18 18:18:50 +0100

    Refactored UI components for improved accessibility and user experience

--- src/common/Contact/Contact.jsx
+++ src/common/Contact/Contact.jsx
@@ -7,19 +7,28 @@ export const Contact = () => {
     return (
         <div className="flex justify-end">
             <div className="flex space-x-5 items-center">
-                <div className="contact-icon">
+                <div className="contact-icon p-2 rounded-md hover:bg-[rgb(86,232,140)] transition duration-300 flex items-center justify-center">
                     <a href="mailto:ladronbravovlc@gmail.com" target="_blank" rel="noopener noreferrer">
-                        <img className="2xl:h-[35px] xl:h-[35px]  lg:h-[30px] md:h-[22px] sm:h-[19px] xs:h-[17px]" src={mailIcon} alt="Mail Icon" draggable="false" />
+                        <img className="2xl:h-[35px] xl:h-[35px] lg:h-[30px] md:h-[22px] sm:h-[19px] xs:h-[17px]" 
+                             src={mailIcon} 
+                             alt="Mail Icon" 
+                             draggable="false" />
                     </a>
                 </div>
-                <div className="contact-icon">
+                <div className="contact-icon p-2 rounded-md hover:bg-[rgb(86,232,140)] transition duration-300 flex items-center justify-center">
                     <a href="https://www.linkedin.com/in/ladronbx/" target="_blank" rel="noopener noreferrer">
-                        <img className="2xl:h-[40px] xl:h-[40px] lg:h-[32px] md:h-[26px] sm:h-[24px] xs:h-[22px]" src={linkedinIcon} alt="LinkedIn Icon" draggable="false" />
+                        <img className="2xl:h-[40px] xl:h-[40px] lg:h-[32px] md:h-[26px] sm:h-[24px] xs:h-[22px]" 
+                             src={linkedinIcon} 
+                             alt="LinkedIn Icon" 
+                             draggable="false" />
                     </a>
                 </div>
-                <div className="contact-icon">
+                <div className="contact-icon p-2 rounded-md hover:bg-[rgb(86,232,140)] transition duration-300 flex items-center justify-center">
                     <a href="https://github.com/ladronbx" target="_blank" rel="noopener noreferrer">
-                        <img className="2xl:h-[40px] xl:h-[40px] lg:h-[32px] md:h-[26px] sm:h-[24px] xs:h-[22px]" src={githubIcon} alt="GitHub Icon" draggable="false" />
+                        <img className="2xl:h-[40px] xl:h-[40px] lg:h-[32px] md:h-[26px] sm:h-[24px] xs:h-[22px]" 
+                             src={githubIcon} 
+                             alt="GitHub Icon" 
+                             draggable="false" />
                     </a>
                 </div>
             </div>
